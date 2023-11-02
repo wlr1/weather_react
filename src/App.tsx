93c2fdb76be0c4412601fd9bd66d46610bfabd0f
wlr1 <uns4d123@gmail.com>
2023-11-02 18:04:31 +0200

    basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import homeGif from "./assets/video/home.gif";
 
 const App = () => {
@@ -7,9 +8,19 @@ const App = () => {
         backgroundImage: "url(" + homeGif + ")",
         backgroundSize: "cover",
         height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <div></div>
+      <div className="grid space-y-3">
+        <div className="bg-opacity-90 bg-neutral-800 w-[1180px] h-[95px] rounded-lg shadow-md shadow-neutral-900">
+          <h1 className="text-white">search bar</h1>
+        </div>
+        <div className="bg-neutral-800 w-[1180px] h-[675px] rounded-lg shadow-2xl shadow-neutral-900 bg-opacity-90">
+          <h1 className="text-4xl text-white ">sdas</h1>
+        </div>
+      </div>
     </div>
   );
 };
